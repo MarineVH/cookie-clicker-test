096b053b9789a135b19be3ad4993e2c2d889f7a1
AudriusGrebliunas <audrius.grebliunas@gmail.com>
2023-10-24 15:01:48 +0200

    Update main.js

--- cookie-clicker/main.js
+++ cookie-clicker/main.js
@@ -14,8 +14,6 @@ let boutonBonus = document.getElementById("btn-bonus")
 
 let flagBonus = false
 
-let valeurClick = 1;
-
 
 
 let valeurAuto = document.getElementById("auto")
@@ -25,80 +23,86 @@ let prixAuto = document.getElementById("clic-auto")
 let boutonAuto = document.getElementById("btn-auto")
 
 
+score.innerHTML = 0
 
 prixMulti.innerHTML = 20
 
 prixAuto.innerHTML = 30
 
-
 valeurMultiplicateur.innerHTML = 1
 
+score.innerHTML = localStorage.getItem("score") || 0
+
+valeurMultiplicateur.innerHTML = localStorage.getItem("multiplicateur") || 1
+
+prixMulti.innerHTML = localStorage.getItem("prixMulti") || 20
+
+valeurAuto.innerHTML = localStorage.getItem("valeurAuto") || 0
 
 
-
-donut.addEventListener("click", function(){
+donut.addEventListener("click", function () {
   console.log("donut")
-  score.innerHTML = (+score.innerHTML)+1*(valeurClick)
+  score.innerHTML = (+score.innerHTML) + 1 * (+valeurMultiplicateur.innerHTML)
 })
 
-boutonMulti.addEventListener("click", ()=>{
-  if ((+score.innerHTML)>=(+prixMulti.innerHTML)){
-    score.innerHTML = (+score.innerHTML)-(+prixMulti.innerHTML)
-    valeurClick = valeurClick*2
-    valeurMultiplicateur.innerHTML = (+valeurMultiplicateur.innerHTML)+1
-    prixMulti.innerHTML = (+prixMulti.innerHTML)*2
+boutonMulti.addEventListener("click", () => {
+  if ((+score.innerHTML) >= (+prixMulti.innerHTML)) {
+    score.innerHTML = (+score.innerHTML) - (+prixMulti.innerHTML)
+    //valeurClick = valeurClick * 2
+    valeurMultiplicateur.innerHTML = (+valeurMultiplicateur.innerHTML) * 2
+    prixMulti.innerHTML = (+prixMulti.innerHTML) * 2
     console.log("yes")
 
   }
-  else{
+  else {
     alert("Vous n'avez pas assez de points")
   }
 })
 
 
-boutonAuto.addEventListener("click", ()=>{
-  if ((+score.innerHTML)>=(+prixAuto.innerHTML)){
-    valeurAuto.innerHTML = (+valeurAuto.innerHTML)+1
-    score.innerHTML = (+score.innerHTML)-30
+boutonAuto.addEventListener("click", () => {
+  if ((+score.innerHTML) >= (+prixAuto.innerHTML)) {
+    valeurAuto.innerHTML = (+valeurAuto.innerHTML) + 1
+    score.innerHTML = (+score.innerHTML) - 30
   }
-  else{
+  else {
     alert("Vous n'avez pas assez de points")
   }
 })
 
-setInterval( ()=>{
-  score.innerHTML = (+score.innerHTML)+(+valeurAuto.innerHTML)
+setInterval(() => {
+  score.innerHTML = (+score.innerHTML) + (+valeurAuto.innerHTML)
 }, 1000)
 
-setInterval( ()=>{
-  if ((+score.innerHTML)>=(+prixAuto.innerHTML)){
-  boutonAuto.style.backgroundColor = "#98FF98"
-  boutonAuto.style.cursor = "pointer"
+setInterval(() => {
+  if ((+score.innerHTML) >= (+prixAuto.innerHTML)) {
+    boutonAuto.style.backgroundColor = "#98FF98"
+    boutonAuto.style.cursor = "pointer"
 
-}
-else{
-  boutonAuto.style.backgroundColor = "#888888"
-  boutonAuto.style.cursor = "not-allowed"
-}
+  }
+  else {
+    boutonAuto.style.backgroundColor = "#888888"
+    boutonAuto.style.cursor = "not-allowed"
+  }
 }, 10)
 
-setInterval( ()=>{
-  if ((+score.innerHTML)>=(+prixMulti.innerHTML)){
-  boutonMulti.style.backgroundColor = "#98FF98"
-  boutonMulti.style.cursor = "pointer"
+setInterval(() => {
+  if ((+score.innerHTML) >= (+prixMulti.innerHTML)) {
+    boutonMulti.style.backgroundColor = "#98FF98"
+    boutonMulti.style.cursor = "pointer"
 
-}
-else{
-  boutonMulti.style.backgroundColor = "#888888"
-  boutonMulti.style.cursor = "not-allowed"
-}
+  }
+  else {
+    boutonMulti.style.backgroundColor = "#888888"
+    boutonMulti.style.cursor = "not-allowed"
+  }
 }, 10)
 
 
-function timerBonus(){
+function timerBonus() {
   flagBonus = false
   boutonBonus.style.backgroundColor = "#888888"
-  boutonBonus.style.cursor="not-allowed"
+  boutonBonus.style.cursor = "not-allowed"
   let timerElement = document.getElementById("timer");
   let remainingTime = 60;
   timerElement.textContent = remainingTime + " secondes jusqu'à activation";
@@ -115,37 +119,55 @@ function timerBonus(){
       flagBonus = true;
       attachBonusClickListener()
     }
-}, 1000)}
-
-
-
-
-function attachBonusClickListener(){
-if (flagBonus == true){
-boutonBonus.addEventListener("click", () => {
-  while (flagBonus==true){
-  valeurClick = valeurClick * 7;
-  
-
-  console.log("BONUS TIME");
-  const timerElement = document.getElementById("timer");
-  let remainingTime = 30;
-  timerElement.textContent = remainingTime + " secondes";
-  timerInterval = setInterval(() => {
-    remainingTime -= 1;
-    timerElement.textContent = remainingTime + " secondes";
-
-    if (remainingTime <= 0) {
-
-      clearInterval(timerInterval);
-      timerElement.textContent = "";
-      valeurClick = valeurClick / 7;
-      timerBonus()
-    }
   }, 1000)
-  flagBonus = false}
-})}}
+}
+
+
+
+
+
+function attachBonusClickListener() {
+  if (flagBonus == true) {
+    boutonBonus.addEventListener("click", () => {
+      while (flagBonus == true) {
+        valeurClick = valeurClick * 7;
+
+
+        console.log("BONUS TIME");
+        const timerElement = document.getElementById("timer");
+        let remainingTime = 30;
+        timerElement.textContent = remainingTime + " secondes";
+        timerInterval = setInterval(() => {
+          remainingTime -= 1;
+          timerElement.textContent = remainingTime + " secondes";
+
+          if (remainingTime <= 0) {
+
+            clearInterval(timerInterval);
+            timerElement.textContent = "";
+            valeurClick = valeurClick / 7;
+            timerBonus()
+          }
+        }, 1000)
+        flagBonus = false
+      }
+    })
+  }
+}
 
 timerBonus()
 
-console.log("test")
+
+setInterval(() =>   {
+  let scoreSauvegarde = parseInt(score.innerHTML, 10);
+  localStorage.setItem("score", scoreSauvegarde)
+
+  let multiSauvegarde = parseInt(valeurMultiplicateur.innerHTML, 10)
+  localStorage.setItem("multiplicateur", multiSauvegarde)
+
+  let prixMultiSauvegarde = parseInt(prixMulti.innerHTML, 10)
+  localStorage.setItem("prixMulti", prixMultiSauvegarde)
+
+  let valeurAutoSauvegarde = parseInt(valeurAuto.innerHTML, 10)
+  localStorage.setItem("valeurAuto", valeurAutoSauvegarde)
+}, 10)
